533c62a6396b1229f508cc138a377c01bb3f3162
Marc D Anderson <marc.anderson@sympraxisconsulting.com>
2014-11-30 01:05:17 -0500

    First commit for 2014.02

---
+++ jquery.SPServices-2014.02 Intellisense.js
@@ -0,0 +1,27 @@
+/*
+ * SPServices - Work with SharePoint's Web Services using jQuery
+ * Version 2014.01
+ * @requires jQuery v1.8 or greater - jQuery 1.10.x recommended
+ *
+ * Copyright (c) 2009-2013 Sympraxis Consulting LLC
+ * Examples and docs at:
+ * http://spservices.codeplex.com
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+/*
+ * @description Work with SharePoint's Web Services using jQuery
+ * @type jQuery
+ * @name SPServices
+ * @category Plugins/SPServices
+ * @author Sympraxis Consulting LLC/marc.anderson@sympraxisconsulting.com
+ * @intellisense added by Daniel Yee @Daniel_the_Nerd
+ */
+
+intellisense.annotate(jQuery, {
+	'SPServices': function() {
+		/// <signature>
+		///   <summary>This is the core function of the library, which you can use to make Ajax calls to the SharePoint Web Services. Note: As of version 2013.01, all calls return a  jQuery deferred object aka a promise.</summary>
+		/// </signature>
+	}
+});
