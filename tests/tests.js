d0c24d4ebee6db078ceb95fded9ed5a5cd0894c8
Marc D Anderson <marc.anderson@sympraxisconsulting.com>
2015-11-23 15:04:32 -0500

    Began adding specific tests

--- tests/tests.js
+++ tests/tests.js
@@ -2,6 +2,13 @@
  * Created by Marc D Anderson on 11/23/2015.
  */
 
-QUnit.test( "hello test", function( assert ) {
-    assert.ok( 1 == "1", "Passed!" );
+QUnit.test( "SPGetCurrentUser", function( assert ) {
+    var currentUser = $().SPServices.SPGetCurrentUser();
+    assert.ok( currentUser !== undefined, "Passed!" );
+});
+
+
+QUnit.test( "SPGetCurrentSite", function( assert ) {
+    var currentSite = $().SPServices.SPGetCurrentSite();
+    assert.ok( currentSite !== undefined, "Passed! SPCurrentSite =::" + currentSite + "::" );
 });
