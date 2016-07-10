0c7b4338b2c7d281408906459f88f37b67cb3baf
Marc D Anderson <marc.anderson@sympraxisconsulting.com>
2016-07-10 13:55:23 -0400

    Added support for 2013 people pickers in SPFindPeoplePicker

--- src/value-added/SPFindPeoplePicker.js
+++ src/value-added/SPFindPeoplePicker.js
@@ -31,61 +31,94 @@ define([
             return $(this).contents().eq(0).text() === opt.peoplePickerDisplayName;
         }).closest("tr");
 
+        // Use this to label the type of people picker we're dealing with
+        var thisPPType;
+
+        // SP2010 and earlier
         var thisContents = thisRow.find("div[name='upLevelDiv']");
-        var thisCheckNames = thisRow.find("img[Title='" + opt.checkNamesPhrase + "']:first");
-
-        // If a value was provided, set the value
-        if (opt.valueToSet.length > 0) {
-            thisContents.html(opt.valueToSet);
+        if(thisContents.length !== 0) {
+            thisPPType === "old";
+            // 2013+
+        } else {
+            thisPPType === "new";
+            thisContents = thisRow.find(".sp-peoplepicker-topLevel[title='" + opt.peoplePickerDisplayName + "']");
         }
 
-        // If checkName is true, click the check names icon
-        if (opt.checkNames) {
-            thisCheckNames.click();
-        }
-        var thisCurrentValue = $.trim(thisContents.text());
+        if(thisPPType === "old") {
 
-        // Parse the entity data
-        var dictionaryEntries = [];
+            var thisCheckNames = thisRow.find("img[Title='" + opt.checkNamesPhrase + "']:first");
 
-        // IE
-        thisContents.children("span").each(function () {
-
-            // Grab the entity data
-            var thisData = $(this).find("div[data]").attr("data");
-
-            var dictionaryEntry = {};
-
-            // Entity data is only available in IE
-            if (typeof thisData !== "undefined") {
-                var arrayOfDictionaryEntry = $.parseXML(thisData);
-                var $xml = $(arrayOfDictionaryEntry);
-
-                $xml.find("DictionaryEntry").each(function () {
-                    var key = $(this).find("Key").text();
-                    dictionaryEntry[key] = $(this).find("Value").text();
-                });
-                dictionaryEntries.push(dictionaryEntry);
-                // For other browsers, we'll call GetUserInfo to get the data
-            } else {
-                $().SPServices({
-                    operation: "GetUserInfo",
-                    async: false,
-                    cacheXML: true,
-                    userLoginName: $(this).attr("title"),
-                    completefunc: function (xData) {
-
-                        $(xData.responseXML).find("User").each(function () {
-
-                            $.each(this.attributes, function (i, attrib) {
-                                dictionaryEntry[attrib.name] = attrib.value;
-                            });
-                            dictionaryEntries.push(dictionaryEntry);
-                        });
-                    }
-                });
+            // If a value was provided, set the value
+            if (opt.valueToSet.length > 0) {
+                thisContents.html(opt.valueToSet);
             }
-        });
+
+            // If checkName is true, click the check names icon
+            if (opt.checkNames) {
+                thisCheckNames.click();
+            }
+            var thisCurrentValue = $.trim(thisContents.text());
+
+            // Parse the entity data
+            var dictionaryEntries = [];
+
+            // IE
+            thisContents.children("span").each(function () {
+
+                // Grab the entity data
+                var thisData = $(this).find("div[data]").attr("data");
+
+                var dictionaryEntry = {};
+
+                // Entity data is only available in IE
+                if (typeof thisData !== "undefined") {
+                    var arrayOfDictionaryEntry = $.parseXML(thisData);
+                    var $xml = $(arrayOfDictionaryEntry);
+
+                    $xml.find("DictionaryEntry").each(function () {
+                        var key = $(this).find("Key").text();
+                        dictionaryEntry[key] = $(this).find("Value").text();
+                    });
+                    dictionaryEntries.push(dictionaryEntry);
+                    // For other browsers, we'll call GetUserInfo to get the data
+                } else {
+                    $().SPServices({
+                        operation: "GetUserInfo",
+                        async: false,
+                        cacheXML: true,
+                        userLoginName: $(this).attr("title"),
+                        completefunc: function (xData) {
+
+                            $(xData.responseXML).find("User").each(function () {
+
+                                $.each(this.attributes, function (i, attrib) {
+                                    dictionaryEntry[attrib.name] = attrib.value;
+                                });
+                                dictionaryEntries.push(dictionaryEntry);
+                            });
+                        }
+                    });
+                }
+            });
+        } else {
+
+            // Thanks to the post https://jasonscript.wordpress.com/2013/08/07/javascript-and-working-with-the-sharepoint-2013-people-picker/ for a leg up on this
+            var ppEditor = thisContents.find("[title='" + opt.peoplePickerDisplayName + "']");
+            var spPP = SPClientPeoplePicker.SPClientPeoplePickerDict[thisContents[0].id];
+            var ppHidden = thisContents.find("[id$='ClientPeoplePicker_HiddenInput']");
+
+            // We don't have a checknames in new people pickers
+            var thisCheckNames = null;
+
+            // If a value was provided, set the value
+            if (opt.valueToSet.length > 0) {
+                ppEditor.val(opt.valueToSet);
+                spPP.AddUnresolvedUserFromEditor(true);
+            }
+
+            // Get the entity data
+            var dictionaryEntries = ppHidden.value;
+        }
 
         return {
             row: thisRow,
