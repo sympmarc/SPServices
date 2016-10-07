a00a480ceaa0e1869ead1573ee3184117342df36
Josh McCarty <josh@joshmccarty.com>
2016-10-06 23:08:09 -0700

    Create frontpage template, additional helpers and partials

--- docs/templates/helpers/is_active.js
+++ docs/templates/helpers/is_active.js
@@ -1,5 +1,11 @@
 var path = require('path');
 
+/**
+ * Indicates that the current link is for the current page (via an .active class)
+ * @param  {String} current The path to the current page
+ * @param  {String} target  The path to the link target
+ * @return {String}         Returns 'active' if the link is for the current page
+ */
 module.exports = function(current, target) {
    // normalize and remove starting slash from path
    if(!current || !target){
--- docs/templates/helpers/relative_path.js
+++ docs/templates/helpers/relative_path.js
@@ -1,8 +1,12 @@
 var path = require('path');
 
 /**
- * Create Handlebars helper to generate relative links for navigation.
+ * Provides a relative link for navigation.
  * See https://github.com/unstoppablecarl/metalsmith-navigation/blob/master/examples/generic/build.js
+ *
+ * @param {String} current  The path of the current page
+ * @param {String} target   The path of the target page
+ * @return {String}         The path to the target page relative to the current page
  */
 module.exports = function(current, target) {
    // normalize and remove starting slash from path
